401386dc233110cfecec4de76790d5b77b2994b6
kerem <keremtiryaki@gmail.com>
2024-02-10 01:41:06 -0500

    Add Footer and Navbar Links components

---
+++ packages/ui/c/navbar/Links.svelte
@@ -0,0 +1,10 @@
+<!-- <li><a href="/" class="link">Button</a></li>
+<li><a href="/" class="link link-neutral">Neutral</a></li>
+<li><a href="/" class="link link-primary">Primary</a></li> -->
+<li><a href="/" class="link link-secondary">Secondary</a></li>
+<li><a href="/" class="link link-secondary">Secondary</a></li>
+<li><a href="/" class="link link-secondary">Secondary</a></li>
+<li><a href="/" class="link link-secondary">Secondary</a></li>
+<!-- <li><a href="/" class="link link-accent">Accent</a></li>
+<li><a href="/" class="link link-ghost">Ghost</a></li>
+<li><a href="/" class="link link-link">Link</a></li> -->
