474bf9900256df47111c10038f3060b749a36fa3
kerem <keremtiryaki@gmail.com>
2024-02-10 01:42:01 -0500

    layout nav, hero, footer Add font-awesome stylesheet and update dev script port

--- apps/single-location-web/src/routes/+page.svelte
+++ apps/single-location-web/src/routes/+page.svelte
@@ -1,4 +1,35 @@
-<script lang="ts">
+<div
+	class="hero min-h-screen"
+	style="background-image: url(https://boring-hamilton-6cb628.netlify.app/bg.jpg);"
+>
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div
+		class="hero-content text-center text-neutral-content max-w-3xl w-full rounded-lg"
+	>
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Order now!</h1>
+			<!-- <p class="mb-5">Find location near you</p> -->
+			<!-- <button class="btn btn-primary">Find</button> -->
+
+			<div class="join min-w-full mt-10">
+				<div>
+					<div>
+						<input
+							class="input input-bordered input-lg w-full max-w-xs join-item"
+							placeholder="Your address"
+						/>
+					</div>
+				</div>
+				<div class="indicator">
+					<!-- <span class="indicator-item badge badge-ghost">😋</span> -->
+					<button class="btn btn-primary btn-lg join-item">Find</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- <script lang="ts">
 	import Sample from "ui/c/Sample.svelte";
 	import MyCounterButton from "ui/c/MyCounterButton.svelte";
 </script>
@@ -8,4 +39,4 @@ SAMPLE COMPONENT:<Sample />
 <hr />
 <br />
 MyCounterButton COMPONENT:
-<MyCounterButton />
+<MyCounterButton /> -->
